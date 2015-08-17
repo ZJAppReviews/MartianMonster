3b992a132b7cff29a20cea4876e7f46f408877e8
vik denic <vik.denic@gmail.com>
2015-08-16 22:14:48 -0500

    Initial Commit

---
+++ MartianMonster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MartianMonster
+//
+//  Created by Vik Denic on 8/16/15.
+//  Copyright (c) 2015 vikzilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MartianMonster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MartianMonster
+//
+//  Created by Vik Denic on 8/16/15.
+//  Copyright (c) 2015 vikzilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
