5423d8efac6f8222d911d67bfb4ea407c2435771
vik denic <vik.denic@gmail.com>
2015-08-16 23:39:04 -0500

    mvp complete

--- MartianMonster/ViewController.m
+++ MartianMonster/ViewController.m
@@ -7,21 +7,62 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIButton *topButton;
+@property (strong, nonatomic) IBOutlet UIButton *middleButton;
+@property (strong, nonatomic) IBOutlet UIButton *bottomButton;
+
 @end
 
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+@implementation ViewController {
+    SystemSoundID soundEffect;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.middleButton.titleLabel.numberOfLines = 1;
+    self.middleButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    self.middleButton.titleLabel.lineBreakMode = NSLineBreakByClipping;
+
+    self.bottomButton.titleLabel.numberOfLines = 1;
+    self.bottomButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    self.bottomButton.titleLabel.lineBreakMode = NSLineBreakByClipping;
+
+    [[UIApplication sharedApplication] setStatusBarHidden:YES
+                                            withAnimation:UIStatusBarAnimationFade];
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+- (IBAction)onTopButtonTapped:(UIButton *)sender
+{
+    [self playSoundWithName:@"BlastOffBite"];
+}
+
+- (IBAction)onMiddleButtonTapped:(UIButton *)sender
+{
+    [self playSoundWithName:@"SpeedOfYourRocketBite"];
+}
+
+- (IBAction)onBottomButtonTapped:(UIButton *)sender
+{
+    [self playSoundWithName:@"TripIsShortBite"];
+}
+
+-(void)playSoundWithName:(NSString *)soundName
+{
+    NSString *soundPath = [[NSBundle mainBundle] pathForResource:soundName ofType:@"m4a"];
+    NSURL *soundURL = [NSURL fileURLWithPath:soundPath];
+    AudioServicesCreateSystemSoundID(CFBridgingRetain(soundURL), &soundEffect);
+    AudioServicesPlaySystemSound(soundEffect);
 }
 
 @end
