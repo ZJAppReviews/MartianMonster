62de478e9d5ff6d0be9b93ad992a2d9fb95efa46
vik denic <vik.denic@gmail.com>
2016-02-17 22:29:36 -0600

    document code

--- MartianMonster/LayoutManager.h
+++ MartianMonster/LayoutManager.h
@@ -9,11 +9,37 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+/** The LayoutManager is responsible for tweaking the layout of views (namely the MenuCollectionView). There are certain cases where the desired spacing could not be achieved solely in Storyboard. And so the LayoutManager provides information on spacing and insets for situations such as landscape mode, iPads, iPhone 4, and iPhone 6 plus. It leverages the Device Type Category on UIDevice to distinguish between devices.
+ *  @class LayoutManager
+ */
 @interface LayoutManager : NSObject
 
-+(UIEdgeInsets) edgeInsetsForMenu;
-+(CGFloat) menuMinLineSpacingIpad;
-+(CGFloat) menuMinLineSpacingIphoneLandscape;
-+(CGFloat) menuMinLineSpacingIphone4;
+/**
+ *  Provides the proper edgeInsets for the MenuCollectionView
+ *
+ *  @return the proper edgeInsets for the MenuCollectionView
+ */
++(UIEdgeInsets)edgeInsetsForMenu;
+
+/**
+ *  provides the proper minimumLineSpacing between menu items for all iPads
+ *
+ *  @return the proper minimumLineSpacing between menu items for all iPads
+ */
++(CGFloat)menuMinLineSpacingIpad;
+
+/**
+ *  provides the proper minimumLineSpacing between menu items for all iPhones when in landscape orientation
+ *
+ *  @return the proper minimumLineSpacing between menu items for all iPhones when in landscape orientation
+ */
++(CGFloat)menuMinLineSpacingIphoneLandscape;
+
+/**
+ *  provides the proper minimumLineSpacing between menu items for the iPhone 4 when in landscape orientation
+ *
+ *  @return the proper minimumLineSpacing between menu items for the iPhone 4 when in landscape orientation
+ */
++(CGFloat)menuMinLineSpacingIphone4;
 
 @end
