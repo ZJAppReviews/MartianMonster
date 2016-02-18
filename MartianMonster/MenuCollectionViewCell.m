d984596207d21421c6626a88be7339ccc8263d55
vik denic <vik.denic@gmail.com>
2016-02-17 19:25:07 -0600

    do not need dispatch to main thread in cell setup; was causing minor UI bug

--- MartianMonster/MenuCollectionViewCell.m
+++ MartianMonster/MenuCollectionViewCell.m
@@ -22,7 +22,6 @@
 }
 
 -(void)setUp {
-    dispatch_async(dispatch_get_main_queue(), ^{
         if (self.isPlaying) {
             [self.menuButton.layer addAnimation:[CABasicAnimation pulsateAnimation]  forKey:@"pulse"];
             self.menuButton.backgroundColor = [UIColor customTransluscentWhite];
@@ -32,7 +31,6 @@
             self.menuButton.backgroundColor = [UIColor customTransluscentDark];
             self.menuButton.isAnimating = NO; //controls highlighting w/ touchevents
         }
-    });
 }
 
 -(void)setIsPlaying:(BOOL)isPlaying {
