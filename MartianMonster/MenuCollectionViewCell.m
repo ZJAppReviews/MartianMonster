c5739c2948d7f2958da1c93e0a8696ed04eee633
vik denic <vik.denic@gmail.com>
2016-02-17 22:50:27 -0600

    set corner radius when cell is reloaded
    solves bug when going between split screen / multitasking and regular app on ipad

--- MartianMonster/MenuCollectionViewCell.m
+++ MartianMonster/MenuCollectionViewCell.m
@@ -22,15 +22,17 @@
 }
 
 -(void)setUp {
-        if (self.isPlaying) {
-            [self.menuButton.layer addAnimation:[CABasicAnimation pulsateAnimation]  forKey:@"pulse"];
-            self.menuButton.backgroundColor = [UIColor customTransluscentWhite];
-            self.menuButton.isAnimating = YES;
-        } else {
-            [self.menuButton.layer removeAllAnimations];
-            self.menuButton.backgroundColor = [UIColor customTransluscentDark];
-            self.menuButton.isAnimating = NO; //controls highlighting w/ touchevents
-        }
+    if (self.isPlaying) {
+        [self.menuButton.layer addAnimation:[CABasicAnimation pulsateAnimation]  forKey:@"pulse"];
+        self.menuButton.backgroundColor = [UIColor customTransluscentWhite];
+        self.menuButton.isAnimating = YES;
+    } else {
+        [self.menuButton.layer removeAllAnimations];
+        self.menuButton.backgroundColor = [UIColor customTransluscentDark];
+        self.menuButton.isAnimating = NO; //controls highlighting w/ touchevents
+    }
+    self.menuButton.clipsToBounds = YES;
+    self.menuButton.layer.cornerRadius = self.bounds.size.width / 2;
 }
 
 -(void)setIsPlaying:(BOOL)isPlaying {
